eb3f8367b747b8d3c874ea8746aa2952e793e7b5
asnaweevixvify <asnaweeezor@gmail.com>
2025-05-10 06:10:56 +0700

    check

--- src/list.jsx
+++ src/list.jsx
@@ -3,19 +3,26 @@ import './App.css'
 
 function List(props){
     const text = props.textInput
+    const [status,setStatus] = useState(false)
+    const [idsoft,setIdSoft] = useState('')
     return(
         <ul>
             {text.map((e,index)=>{
-                return(<li key={index}>{e}
+                return(<li key={index} className={idsoft === index ? status ? 'soft' : 'show' : null}>{e}
                     <div className='icon'>
                         <i className="fa-solid fa-pen fa-xs" onClick={()=> props.editfunction(e,index)}></i> 
                         <i className="fa-solid fa-trash fa-xs" onClick={()=>props.delfunction(index)}></i>
+                        <input type='checkbox' className='check' onChange={()=>setSoft(index)}></input>
                     </div>
                     </li> 
                 )
             })}
         </ul>
     )
+    function setSoft(i){
+        setStatus(!status)
+        setIdSoft(i)
+    }
 }
 
 export default List
